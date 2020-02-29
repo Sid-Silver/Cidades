8a5b43b955b2243d092ac7384b7026dd4ccaa053
= <=>
2020-02-29 20:43:18 -0300

    criando um console dizendo o nome das cidades do litoral sul

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+        }
+    }
+}
